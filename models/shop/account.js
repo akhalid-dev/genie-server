2eafeb6e18cde05c0eabeae2f4037abf467725b8
akhalid7 <akhal022@uottawa.ca>
2020-04-14 01:06:10 -0400

    Page Parser Complete

--- models/shop/account.js
+++ models/shop/account.js
@@ -6,34 +6,62 @@ const parser = new rss({
 
 const cheerio = require('cheerio');
 
-const parseStore = (user, site, proxy='') => {
-    let link = "http://www.ebay." + site + "/sch/rss/m.html?_ssn=" + user + "&_dmd=7" + "&_ipg=200" + "&_rss=1" + "&_pgn=1";
-    parsePage(link);
+const parseStore = (user, site, proxy='', delay) => {
+    let link = proxy + "http://www.ebay." + site + "/sch/rss/m.html?_ssn=" + user + "&_dmd=7" + "&_ipg=200" + "&_rss=1" + "&_pgn=1";
+    parsePage({link: link, modified: false}, {}, delay)
+    .then((props, items) => {
+        console.log(props)
+        if(props.modified) {
+            //const newLink = link + 1;
+            // parsePage({link: newLink, modified: false}, items, delay)
+        } else {
+
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 }
 
-const parsePage = link => {
+const parsePage = (props, items={}, delay=5000) => { // props {link: '', modified: ''}
     return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            parser.parseURL(props.link, (err, feed) => {
+                if (err) reject(err);
 
+                feed.items.forEach(entry => {     
+                    //Item number from URL:
+                    let re = /\?[0-9]*/; 
+                    const itemNumber = reverseString(re.exec(reverseString(entry.link))[0]).replace('?','');
+                    
+                    const url = entry.link;
+                    const title = entry.title;
+                    const $ = cheerio.load(entry.content, {xmlMode: true});
+                    const imageURL = $("img").attr('src');
+                    const price = $("strong").text();
+                    
+                    if (!items.hasOwnProperty(itemNumber)) {
+                        items[itemNumber] = ({
+                            url: url,
+                            title: title,
+                            imageURL: imageURL,
+                            price: price,
+                            itemNumber: itemNumber
+                        });
+                        props.modified = true;
+                    }
+                })
+                resolve(props, items);
+            });
+        }, delay + Math.random() * 5000);
     })
-    parser.parseURL(link, (err, feed) => {
-        if (err) throw err;
-        feed.items.forEach(function(entry) {     
-            //Item number from URL:
-            let re = /\?[0-9]*/; 
-            const itemNumber = reverseString(re.exec(reverseString(entry.link))[0]).replace('?','');
-            
-            const link = entry.link;
-            const title = entry.title;
-            const $ = cheerio.load(entry.content, {xmlMode: true});
-            const imageURL = $("img").attr('src');
-            const price = $("strong").text();
-            
-        })
-    });
 }
 
 const reverseString = (text) => {
     return text.split("").reverse().join("");
 }
+
+parseStore('hockey-grandpa', 'ca', 1);
+//parseStore('achstar91', 'co.uk')
 
 
